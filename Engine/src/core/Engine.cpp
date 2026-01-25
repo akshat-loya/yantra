c763f69acd59e71f672fe986f90fda38504ea9f2
akshat-loya <loya.akshat@gmail.com>
2026-01-25 15:11:06 +0530

    Adding support for GLAD

--- Engine/src/core/Engine.cpp
+++ Engine/src/core/Engine.cpp
@@ -1,3 +1,9 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
 #include "Engine.h"
 
 int Engine::initialise()
@@ -6,26 +12,32 @@ int Engine::initialise()
  * For now, just to test that OpenGL works
  */
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    // OpenGL context hints (macOS safe)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "LearnOpenGL", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
-
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* To print openGL version
-    * std::cout << glGetString(GL_VERSION) << std::endl;
-    */
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0) {
+        printf("Failed to initialize OpenGL context\n");
+        return -1;
+    }
+
+    // Successfully loaded OpenGL
+    printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
     
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -36,14 +48,14 @@ int Engine::initialise()
         /* Modern OpenGL attempt at drawing a triangle */
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
-        /* Legacy OpenGL for a triangle */
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-1.0f, -1.0f);
-        glVertex2f( 0.0f,  1.0f);
-        glVertex2f( 1.0f, -1.0f);
-        glEnd();
-    
-        
+        /* Legacy OpenGL for a triangle
+        ** glBegin(GL_TRIANGLES);
+        ** glVertex2f(-1.0f, -1.0f);
+        ** glVertex2f( 0.0f,  1.0f);
+        ** glVertex2f( 1.0f, -1.0f);
+        ** glEnd();
+        */
+       
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
